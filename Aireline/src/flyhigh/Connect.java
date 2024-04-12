70682306a0e7f8c5fcf56b092ef950d361735f6a
YOUNESSKELLAL <layyouness@gmail.com>
2024-04-12 18:04:08 +0000

    ..
    login ,menu and mange flights

---
+++ Aireline/src/flyhigh/Connect.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flyhigh;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author HP
+ */
+public class Connect {
+    public Connect(){
+        
+    };
+    PreparedStatement pst = null;
+    public Connection Connect(){
+       // Create a variable for the connection string.
+        
+        Connection con = null;
+        // Declare the JDBC objects.
+        
+
+            try {
+                // Establish the connection.
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_user_database","root","");
+                System.out.print("connection done");
+            }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.print("connection error");
+        } 
+            return con;
+    }
+}
---
+++ Aireline/src/flyhigh/Flyhighmain.java
@@ -0,0 +1,11 @@
+
+package flyhigh;
+
+public class Flyhighmain {
+
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+}
+    
+}
